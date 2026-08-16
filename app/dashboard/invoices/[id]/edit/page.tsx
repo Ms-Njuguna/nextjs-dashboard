74c17fd61a5d1d4aa808f46043f3147eae98556f
Patricia <patricianjuguna084@gmail.com>
2026-08-16 20:22:13 +0300

    update metadata titles in login and homepage

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,15 +2,26 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
-import { Metadata } from 'next';
+import type { Metadata } from 'next';
 
-export const metadata: Metadata = {
-  title: 'Invoices | id | Update',
-};
+export async function generateMetadata({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}): Promise<Metadata> {
+  const { id } = await params;
 
-export default async function Page(props: { params: Promise<{ id: string }> }) {
+  return {
+    title: `Invoice ${id} | Update`,
+  };
+}
+
+export default async function Page(
+  props: { params: Promise<{ id: string }> }
+) {
   const params = await props.params;
   const id = params.id;
+
   const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
@@ -32,6 +43,7 @@ export default async function Page(props: { params: Promise<{ id: string }> }) {
           },
         ]}
       />
+
       <Form invoice={invoice} customers={customers} />
     </main>
   );
